2673b86fd758ce549ffe7570e1fc9475d679e990
Patrick Kenny <github@ptmkenny.com>
2021-08-22 15:37:15 +0900

    initial commit

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,29 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myapp.app',
+  appName: 'elecron-blankscreen',
+  webDir: 'build',
+  bundledWebRuntime: false,
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+    SplashScreen: {
+      // launchShowDuration: 5000,
+      launchAutoHide: false,
+      backgroundColor: '#ffce00',
+      showSpinner: true,
+      iosSpinnerStyle: 'large',
+      androidSpinnerStyle: 'large',
+      spinnerColor: '#007849',
+    }
+  },
+  cordova: {
+    preferences: {
+      BILLING_KEY: "abc"
+    }
+  }
+};
+
+export default config;
